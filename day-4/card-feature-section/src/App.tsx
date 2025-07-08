2112dc3eaabf7c7310452d14d5e68edf66df75f5
Fahed Afzaal <95244868+Fahedafzaal@users.noreply.github.com>
2025-07-08 22:46:27 +0500

    Mobile version complete

--- day-4/card-feature-section/src/App.tsx
+++ day-4/card-feature-section/src/App.tsx
@@ -1,5 +1,5 @@
-import Card from './components/Card';
-import { cards } from './data';
+import Card from "./components/Card";
+import { cards } from "./data";
 // import cartIcon from '/icon-cart.svg'
 
 function App() {
@@ -9,10 +9,18 @@ function App() {
         <div className="typography text-center mt-4">
           <h2>Reliable, efficient delivery</h2>
           <h1>Powered by Technology</h1>
-          <p className="mt-3">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+          <p className="mt-3">
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful
+          </p>
         </div>
 
-        <div className="mt-6 flex flex-col gap-8">
+        <div 
+          className="
+            mt-6 mx-auto
+            flex flex-col 
+            lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:gap-8
+            ">
           {cards.map((card, index) => (
             <Card key={index} {...card} />
           ))}
--- day-4/card-feature-section/src/components/Card.tsx
+++ day-4/card-feature-section/src/components/Card.tsx
@@ -1,9 +1,9 @@
 export default function Card({ title, description, icon, color }: { title: string, description: string, icon: string, color: string }) {
   return (
-    <div className={`typography flex flex-col gap-4 border border-gray-200 border-t-4 border-t-${color} shadow-xl rounded-lg p-6`}>
+    <div className={`typography flex flex-col gap-2 border max-w-sm border-gray-200 border-t-4 border-t-${color} shadow-xl rounded-lg p-6`}>
       <h1>{title}</h1>
-      <p className="font-extralight">{description}</p>
-      <img src={icon} alt={title} />
+      <p className="font-normal">{description}</p>
+      <img src={icon} alt={title} className="w-12 h-12 my-4 self-end" />
     </div>
   );
 }
