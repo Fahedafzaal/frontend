e03c0d99fe6eecf6c7ebb2aa9c5d429b0789e9cb
Fahed Afzaal <95244868+Fahedafzaal@users.noreply.github.com>
2025-07-11 18:47:25 +0500

    svg bugged

--- day-5/testimonials-grid-section/src/App.tsx
+++ day-5/testimonials-grid-section/src/App.tsx
@@ -1,10 +1,58 @@
-function App() {
+import Card from "./components/card";
+import patternQuotation from "/bg-pattern-quotation.svg"
+import { testimonials } from "./data";
 
+function App() {
   return (
     <>
-      
+      <main className="flex flex-col gap-4 p-8 bg-gray-100 lg:h-screen">
+        <div className="max-w-lg lg:grid lg:grid-cols-4 lg:grid-rows-2 lg:gap-8 lg:m-12 lg:max-w-7xl m-auto lg:mx-auto lg:my-auto">
+          <Card
+            name={testimonials[0].name}
+            title={testimonials[0].title}
+            headline={testimonials[0].headline}
+            quote={testimonials[0].quote}
+            image={testimonials[0].image}
+            className="bg-purple-500 text-white order-1 col-span-2 relative overflow-visible"
+          >
+            <div className="hidden lg:block absolute top-6 right-8 w-16 h-16 bg-no-repeat bg-contain pointer-events-none" style={{backgroundImage: `url(${patternQuotation})`}}></div>
+          </Card>
+          <Card
+            name={testimonials[1].name}
+            title={testimonials[1].title}
+            headline={testimonials[1].headline}
+            quote={testimonials[1].quote}
+            image={testimonials[1].image}
+            className="bg-gray-500 text-white order-2"
+          />
+          <Card
+            name={testimonials[2].name}
+            title={testimonials[2].title}
+            headline={testimonials[2].headline}
+            quote={testimonials[2].quote}
+            image={testimonials[2].image}
+            className="bg-white text-black order-4"
+          />
+          <Card
+            name={testimonials[3].name}
+            title={testimonials[3].title}
+            headline={testimonials[3].headline}
+            quote={testimonials[3].quote}
+            image={testimonials[3].image}
+            className="bg-slate text-white col-span-2 order-5"
+          />
+          <Card
+            name={testimonials[4].name}
+            title={testimonials[4].title}
+            headline={testimonials[4].headline}
+            quote={testimonials[4].quote}
+            image={testimonials[4].image}
+            className="bg-white text-black row-span-2 order-3"
+          />
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ day-5/testimonials-grid-section/src/components/card.tsx
@@ -0,0 +1,34 @@
+export default function Card({
+  name,
+  title,
+  headline,
+  quote,
+  image,
+  className = "",
+  children
+}: {
+  name: string;
+  title: string;
+  headline: string;
+  quote: string;
+  image: string;
+  className?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <div
+      className={`card p-8 mb-8 lg:mb-0 rounded-lg shadow-xl ${className}`.trim()}
+    >
+      <div className="flex items-center gap-4">
+        <img src={image} alt={name} className="h-10 w-10 rounded-full" />
+        <div>
+          <h1>{name}</h1>
+          <p>{title}</p>
+        </div>
+      </div>
+      <h2 className="mt-4">{headline}</h2>
+      <p className="mt-4">{quote}</p>
+      {children}
+    </div>
+  );
+}
