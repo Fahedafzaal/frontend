4dbc45933cf5702cd5a4958803d59766af839410
Fahed Afzaal <95244868+Fahedafzaal@users.noreply.github.com>
2025-07-24 20:35:30 +0500

    Basic design complete

--- day-7/newsletter-form/src/App.tsx
+++ day-7/newsletter-form/src/App.tsx
@@ -5,15 +5,16 @@ import listIcon from "/icon-list.svg";
 function App() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center h-screen">
+      <main className="w-screen h-screen md:bg-blue-700 md:flex md:items-center md:justify-center">
+        <div className="flex flex-col items-center justify-center md:flex-row md:p-6 md:bg-white md:rounded-3xl md:max-w-4xl md:mx-auto">
         <div className="card-image">
           <picture>
             <source srcSet={signUpDesktop} media="(min-width: 768px)" />
-            <img src={signUpMobile} className="float-start" alt="Sign Up" />
+            <img src={signUpMobile} alt="Sign Up" />
           </picture>
         </div>
-        <div className="card-content flex flex-col p-6 mt-4 gap-6">
-          <h1 className="text-4xl font-bold">Stay updated!</h1>
+        <div className="card-content flex flex-col p-6 mt-4 gap-6 md:order-first md:mr-4">
+          <h1 className="text-4xl font-bold md:text-6xl">Stay updated!</h1>
           <p className="text-md">
             Join 60,000+ product managers receiving monthly updates on:
           </p>
@@ -46,12 +47,13 @@ function App() {
               </div>
               <button
                 type="submit"
-                className="bg-blue-800 text-white rounded-lg block w-full p-4"
+                className="bg-blue-800 text-white rounded-lg block w-full p-4 hover:bg-gradient-to-r from-pink-500 to-orange-500"
               >
                 Subscribe to monthly newsletter
               </button>
             </form>
           </div>
+        </div>
         </div>
       </main>
     </>
